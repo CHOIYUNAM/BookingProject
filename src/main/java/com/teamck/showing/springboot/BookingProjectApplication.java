83942d05bf7f35e41b639df382cc23d25577d410
김태욱 <44253504+T2us@users.noreply.github.com>
2022-08-11 16:14:17 +0900

    SpringBoot

---
+++ src/main/java/com/teamck/showing/springboot/BookingProjectApplication.java
@@ -0,0 +1,13 @@
+package com.teamck.showing.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingProjectApplication.class, args);
+	}
+
+}
